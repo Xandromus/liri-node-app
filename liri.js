def66b5d83bf879d73e7db57d80227e5ac9e6abb
Xandromus <xanderrapstine@gmail.com>
2018-03-30 15:49:37 -0500

    added twitter functionality

--- liri.js
+++ liri.js
@@ -1,6 +1,29 @@
 require("dotenv").config();
 let fs = require("fs-extra");
 let keys = require("./keys");
+let Twitter = require('twitter');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+//let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+let params = { screen_name: 'basementavatars', count: 20 };
+
+
+let inputString = process.argv;
+let operand = inputString[2];
+
+if (operand === "my-tweets") {
+    client.get('statuses/user_timeline/', params, function (error,
+        tweets, response) {
+        if (!error) {
+            let counter = tweets.length;
+            console.log("The last " + counter + " tweets from " + tweets[0].user.name + " in ascending order:\n" + "-".repeat(60));
+            tweets.forEach(function (tweet) {
+                console.log("\nTweet #" + counter + "\n" + "----------");
+                console.log(tweet.text.replace(/&amp;/g, '&') + "\n");
+                console.log(tweet.created_at + "\n");
+                counter--;
+            });
+        }
+    });
+}
