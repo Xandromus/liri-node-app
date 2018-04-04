11eae100ef1a5ce5d341e215d24b16bd80b1efed
Xandromus <xanderrapstine@gmail.com>
2018-04-03 20:51:18 -0500

    added error checking for spotify and for omdb if no imdb rating is supplied

--- liri.js
+++ liri.js
@@ -61,18 +61,22 @@ function spotifyThis() {
     spotify
         .search({ type: 'track', query: trackName, limit: 1 })
         .then(response => {
-            let trackObj = JSON.parse(JSON.stringify(response.tracks.items[0], null, 2));
-            console.log("\nSpotify Song Information:\n" + "-".repeat(25) + "\n");
-            console.log("Artist(s):\n" + "-".repeat(10));
-            trackObj.artists.forEach(function (artist) {
-                console.log(artist.name);
-            });
-            console.log("\nSong Title:\n" + "-".repeat(11) + "\n" + trackObj.name);
-            console.log("\nAlbum Title:\n" + "-".repeat(12) + "\n" + trackObj.album.name);
-            if (trackObj.preview_url !== null) {
-                console.log("\nPreview Link:\n" + "-".repeat(13) + "\n" + trackObj.preview_url);
+            if (JSON.stringify(response.tracks.items[0]) != undefined) {
+                let trackObj = JSON.parse(JSON.stringify(response.tracks.items[0], null, 2));
+                console.log("\nSpotify Song Information:\n" + "-".repeat(25) + "\n");
+                console.log("Artist(s):\n" + "-".repeat(10));
+                trackObj.artists.forEach(function (artist) {
+                    console.log(artist.name);
+                });
+                console.log("\nSong Title:\n" + "-".repeat(11) + "\n" + trackObj.name);
+                console.log("\nAlbum Title:\n" + "-".repeat(12) + "\n" + trackObj.album.name);
+                if (trackObj.preview_url !== null) {
+                    console.log("\nPreview Link:\n" + "-".repeat(13) + "\n" + trackObj.preview_url);
+                } else {
+                    console.log("\nPreview Link:\n" + "-".repeat(13) + "\nNo preview available for this song");
+                }
             } else {
-                console.log("\nPreview Link:\n" + "-".repeat(13) + "\nNo preview available for this song");
+                console.log("No song found. Try searching again.");
             }
         })
         .catch(err => console.log(err))
@@ -90,7 +94,11 @@ function movieThis() {
                 console.log("\nHere's a summary of the movie you searched for:\n" + "-".repeat(47));
                 console.log("\nTitle:\n" + "-".repeat(6) + "\n" + movieObj.Title);
                 console.log("\nRelease Year:\n" + "-".repeat(13) + "\n" + movieObj.Year);
-                console.log("\n" + movieObj.Ratings[0].Source + " Rating:\n" + "-".repeat(31) + "\n" + movieObj.Ratings[0].Value);
+                if (movieObj.Ratings[1]) {
+                    console.log("\n" + movieObj.Ratings[0].Source + " Rating:\n" + "-".repeat(31) + "\n" + movieObj.Ratings[0].Value);
+                } else {
+                    console.log("\nInternet Movie Database Rating:\n" + "-".repeat(23) + "\nThe selection you entered does not have a rating on IMDB");
+                }
                 if (movieObj.Ratings[1]) {
                     console.log("\n" + movieObj.Ratings[1].Source + " Rating:\n" + "-".repeat(23) + "\n" + movieObj.Ratings[1].Value);
                 } else {
